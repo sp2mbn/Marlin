5e51df32688bc5496512f5040047e6c5faa27851
petrzmax <petrzmax@gmail.com>
2020-08-02 22:37:17 +0200

    Advance_Pause settings tweaked

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -598,7 +598,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 650
+#define EXTRUDE_MAXLENGTH 670
 
 //===========================================================================
 //======================== Thermal Runaway Protection =======================

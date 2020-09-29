b57237d8d16592fef3e279b23898a4a18fdd13b2
petrzmax <petrzmax@gmail.com>
2020-09-29 13:04:55 +0200

    Extruder adjustments & BabyStepping enabled

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -758,7 +758,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 30, 60 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 30, 50 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -835,7 +835,7 @@
  *
  * See https://github.com/synthetos/TinyG/wiki/Jerk-Controlled-Motion-Explained
  */
-#define S_CURVE_ACCELERATION
+//#define S_CURVE_ACCELERATION
 
 //===========================================================================
 //============================= Z Probe Options =============================
